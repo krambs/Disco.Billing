e3ab3f101dc3e94749050e77078a23a05fa7ed0d
Gabe Krambs <krambs@csdisco.com>
2016-02-04 10:40:58 -0600

    Get less compiling

--- Disco.Billing.WebUI/gulpfile.js
+++ Disco.Billing.WebUI/gulpfile.js
@@ -1,11 +1,12 @@
-﻿/// <binding Clean='clean' />
+﻿/// <binding AfterBuild='build' Clean='clean' />
 "use strict";
 
 var gulp = require("gulp"),
     rimraf = require("rimraf"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify");
+    uglify = require("gulp-uglify"),
+    less = require("gulp-less");
 
 var paths = {
     webroot: "./wwwroot/"
@@ -13,29 +14,31 @@ var paths = {
 
 paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.less = paths.webroot + "css/**/*.less";
 paths.css = paths.webroot + "css/**/*.css";
+paths.cssFolder = paths.webroot + "css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
 
-gulp.task("clean:js", function (cb) {
+gulp.task("clean:js", function(cb) {
     rimraf(paths.concatJsDest, cb);
 });
 
-gulp.task("clean:css", function (cb) {
+gulp.task("clean:css", function(cb) {
     rimraf(paths.concatCssDest, cb);
 });
 
 gulp.task("clean", ["clean:js", "clean:css"]);
 
-gulp.task("min:js", function () {
+gulp.task("min:js", function() {
     return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
         .pipe(concat(paths.concatJsDest))
         .pipe(uglify())
         .pipe(gulp.dest("."));
 });
 
-gulp.task("min:css", function () {
+gulp.task("min:css", function() {
     return gulp.src([paths.css, "!" + paths.minCss])
         .pipe(concat(paths.concatCssDest))
         .pipe(cssmin())
@@ -43,3 +46,11 @@ gulp.task("min:css", function () {
 });
 
 gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task("compile:less", function() {
+    return gulp.src(paths.less)
+        .pipe(less())
+        .pipe(gulp.dest(paths.cssFolder));
+});
+
+gulp.task("build", ["compile:less", "min"]);
